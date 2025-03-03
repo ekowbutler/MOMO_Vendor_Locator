95aea8b363e1118e93038dcf837f516821e489f0
ekowbutler <ekowbutler@gmail.com>
2025-03-03 11:22:06 +0000

    added the point

--- momoapp1.py
+++ momoapp1.py
@@ -1,5 +1,9 @@
 import folium
 
 my_area_map = folium.Map(location=[5.5403889, -0.2707222], zoom_start=6)
-
+my_area_map.add_child(
+    folium.Marker(
+        location=[5.54, -0.27], popup="A&C Ventures", icon=folium.Icon(color="blue")
+    )
+)
 my_area_map.save("index.html")
