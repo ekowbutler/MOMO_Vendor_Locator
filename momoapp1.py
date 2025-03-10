3af0fbf3cb01e588ea9effd2da9318a266708d56
ekowbutler <ekowbutler@gmail.com>
2025-03-10 23:49:06 +0000

    user location added

--- momoapp1.py
+++ momoapp1.py
@@ -1,16 +1,19 @@
 import folium
 import pandas
+from folium.plugins import LocateControl
 
 data = pandas.read_csv("locations.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
-Shop_name = list(data["Name"])
+Shop_Locations = list(data["Name"])
 
 map = folium.Map(location=[lat[0], lon[0]], zoom_start=50)
 
+LocateControl(auto_start=True).add_to(map)
+
 fg = folium.FeatureGroup(name="My Area Map")
 
-for lt, ln, nm in zip(lat, lon, Shop_name):
+for lt, ln, nm in zip(lat, lon, Shop_Locations):
     fg.add_child(
         folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color="blue"))
     )
