f5423caf7ad35e57f269659cb9e049d7973dd0c2
ekowbutler <ekowbutler@gmail.com>
2025-03-10 21:05:35 +0000

    added location

--- momoapp1.py
+++ momoapp1.py
@@ -1,4 +1,10 @@
 import folium
+import pandas
+
+data = pandas.read_csv("locations.txt")
+lat = list(data["LAT"])
+lon = list(data["LAT"])
+Shop_name = list(data["NAMe"])
 
 my_area_map = folium.Map(location=[5.5403889, -0.2707222], zoom_start=6)
 my_area_map.add_child(
